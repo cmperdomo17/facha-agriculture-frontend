22bcaf3028d967ed9834473b70b09a493d318680
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-10-05 11:55:15 -0500

    First commit

---
+++ src/app/utils/metadata/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "FACHA Agriculture - NASA Space Apps Challenge 2024",
+    description: "Created by FACHA CREW with Nextjs",
+};
